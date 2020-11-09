4ef8b5dd9e6a0a307ce7654d544ee0af01e8da17
codwerk <louis_widdy@live.fr>
2020-11-09 16:56:51 +0100

    Paramètres des URLs

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.get('/api/v1', (req, res) => {
   res.send('Api Version 1')
 })
 
+app.get('/api/v1/books/:id', (req, res) => {
+  res.send(req.params)
+})
+
+///api/v1/books/4
+
 app.listen(8080, () => {
   console.log('Started on port 8080!');
 })
