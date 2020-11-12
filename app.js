3cc809633a004964c84615aac876a41c887fa5a8
codwerk <louis_widdy@live.fr>
2020-11-12 17:24:23 +0100

    Utilisation du package Morgan

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
 
-//middlewares
+/* middlewares
 app.use( (req, res, next) => {
   console.log('URL : ', req.url);
   next();
-})
+}) */
+
+//morgan
+app.use(morgan('dev'))
 
 app.get('/api', (req, res) => {
   res.send('Root Api')
