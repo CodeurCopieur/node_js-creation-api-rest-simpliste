5fff7b98a26ebdfd67bb23f7e6ec16dbe0a51140
codwerk <louis_widdy@live.fr>
2020-11-13 20:00:44 +0100

    Gérer les paramètres

--- app.js
+++ app.js
@@ -2,15 +2,54 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 
-
-/* middlewares
-app.use( (req, res, next) => {
-  console.log('URL : ', req.url);
-  next();
-}) */
+const members = [
+  {
+    id: 1,
+    name: 'John'
+  },
+  {
+    id: 2,
+    name: 'Louis'
+  },
+  {
+    id: 3,
+    name: 'Leo'
+  }
+]
 
 //morgan
 app.use(morgan('dev'))
+
+//route pour  afficher un membre
+app.get('/api/v1/members/:id', (req, res) => {
+  res.send(members[(req.params.id)-1].name)
+})
+
+//route pour afficher tous les membres ou bien choisir le nombre de membre à afficher (ex : /api/v1/members?max=2)
+app.get('/api/v1/members', (req, res) => {
+  if(req.query.max != undefined && req.query.max > 0) {
+    res.send(members.slice(0, req.query.max))
+  } else {
+    res.send(members)
+  }
+})
+
+app.get('/api/v1/members/:id', (req, res) => {
+  res.send(members[(req.params.id)-1].name)
+})
+
+app.listen(8080, () => {
+  console.log('Started on port 8080!');
+})
+
+
+/*
+
+Middlewares
+app.use( (req, res, next) => {
+  console.log('URL : ', req.url);
+  next();
+})
 
 app.get('/api', (req, res) => {
   res.send('Root Api')
@@ -23,9 +62,6 @@ app.get('/api/v1', (req, res) => {
 app.get('/api/v1/books/:id', (req, res) => {
   res.send(req.params)
 })
+// /api/v1/books/4
 
-///api/v1/books/4
-
-app.listen(8080, () => {
-  console.log('Started on port 8080!');
-})
+*/
